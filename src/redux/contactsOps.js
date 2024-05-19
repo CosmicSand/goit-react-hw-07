dc1536e7487943c9a6e10b37c33cb5eedf0d2fff
CosmicSand <skyjlen@gmail.com>
2024-05-19 18:54:28 +0300

    Update contactsOps.js

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -32,7 +32,6 @@ export const deleteContact = createAsyncThunk(
   async (id, thunkAPI) => {
     try {
       const response = await axios.delete(`/contacts/${id}`);
-      console.log(response.data);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
